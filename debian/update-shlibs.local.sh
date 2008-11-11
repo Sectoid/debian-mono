d7a27f771f3dcef1c48ebad66e8d23e70f68012d
Mirco Bauer <meebey@debian.org>
2008-11-11 21:51:28 +0000

    flush 2.0-1

--- debian/update-shlibs.local.sh
+++ debian/update-shlibs.local.sh
@@ -5,6 +5,8 @@ UPVERSION=$(echo $VERSION | sed 's,-.*,,' | sed 's,+dfsg,,')
 
 echo -n "Updating shlibs.local for Mono $UPVERSION..."
 
+# libgamin-dev, libcupsys2-dev, librsvg2-dev, libgtk2.0-dev, libgnomeui-dev
+
 cp debian/shlibs.local debian/shlibs.local.backup
 rm -f debian/shlibs.local.new
 
@@ -13,12 +15,12 @@ echo "libMonoPosixHelper 0 libmono0 (>= $UPVERSION)"	>> debian/shlibs.local.new
 echo "libgdiplus 0 libgdiplus (>= $UPVERSION)"		>> debian/shlibs.local.new
 echo "libgluezilla 0 libgluezilla (>= $UPVERSION)"	>> debian/shlibs.local.new
 
-grep --no-filename "libcairo 2"		/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new
-grep --no-filename "libfbclient 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new
-grep --no-filename "libsqlite 0"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new
-grep --no-filename "libsqlite3 0"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new
-grep --no-filename "libasound 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new
-grep --no-filename "libgda-2 3"		/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new
+grep --no-filename "^libcairo 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libcairo"
+grep --no-filename "^libfbclient 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libfbclient"
+grep --no-filename "^libsqlite 0"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libsqlite"
+grep --no-filename "^libsqlite3 0"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libsqlite3"
+grep --no-filename "^libasound 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libasound"
+grep --no-filename "^libgda-2 3"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libgda-2"
 
 cp debian/shlibs.local.new debian/shlibs.local
 
