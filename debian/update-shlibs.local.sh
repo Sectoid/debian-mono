534c6f80122f5d9e3790aba2e64fd6e7c750aed3
Mirco Bauer <meebey@debian.org>
2009-02-24 23:24:20 +0000

    * debian/control:     + Removed libgamin-dev, libcups2-dev, librsvg2-dev, libgtk2.0-dev and       libgnomeui-dev from build-deps-indep as those packages are only needed       to generate the debian/shlibs.local file using the       debian/update-shlibs.local.sh script (the script checks now if those       are installed). This extended hack is needed as shared-mime-info in       unstable conflicts with gnome < 2.24 and gnome 2.24 is blocked by       the Mono 2.0 transition.   * debian/shlibs.local:     + Updated using the debian/update-shlibs.local.sh script.

--- debian/update-shlibs.local.sh
+++ debian/update-shlibs.local.sh
@@ -2,25 +2,47 @@
 
 VERSION=$(dpkg-parsechangelog | grep ^Vers | cut -d\  -f2)
 UPVERSION=$(echo $VERSION | sed 's,-.*,,' | sed 's,+dfsg,,')
+MAJOR_MINOR_UPVERSION=$(perl -e '$_=pop; print m/^(\d+\.\d+)/g;' $UPVERSION)
 
+dpkg-checkbuilddeps -d "\
+	libcairo2-dev, \
+	firebird2.0-dev, \
+	libsqlite0-dev, \
+	libsqlite3-dev, \
+	libasound2-dev, \
+	libgda2-dev, \
+	libgamin-dev, \
+	libcups2-dev, \
+	librsvg2-dev, \
+	libgtk2.0-dev, \
+	libgnomeui-dev
+"
+	
 echo -n "Updating shlibs.local for Mono $UPVERSION..."
-
-# libgamin-dev, libcupsys2-dev, librsvg2-dev, libgtk2.0-dev, libgnomeui-dev
 
 cp debian/shlibs.local debian/shlibs.local.backup
 rm -f debian/shlibs.local.new
 
 # libs that don't ship shlibs
-echo "libMonoPosixHelper 0 libmono0 (>= $UPVERSION)"	>> debian/shlibs.local.new
-echo "libgdiplus 0 libgdiplus (>= $UPVERSION)"		>> debian/shlibs.local.new
-echo "libgluezilla 0 libgluezilla (>= $UPVERSION)"	>> debian/shlibs.local.new
+echo "libMonoPosixHelper 0 libmono0 (>= $MAJOR_MINOR_UPVERSION)"	>> debian/shlibs.local.new
+echo "libgdiplus 0 libgdiplus (>= $MAJOR_MINOR_UPVERSION)"		>> debian/shlibs.local.new
+echo "libgluezilla 0 libgluezilla (>= $MAJOR_MINOR_UPVERSION)"		>> debian/shlibs.local.new
 
-grep --no-filename "^libcairo 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libcairo"
-grep --no-filename "^libfbclient 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libfbclient"
-grep --no-filename "^libsqlite 0"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libsqlite"
-grep --no-filename "^libsqlite3 0"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libsqlite3"
-grep --no-filename "^libasound 2"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libasound"
-grep --no-filename "^libgda-2 3"	/var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve libgda-2"
+for SONAME in \
+	"^libcairo 2" \
+	"^libfbclient 2" \
+	"^libsqlite 0" \
+	"^libsqlite3 0" \
+	"^libasound 2" \
+	"^libgda-2 3" \
+	"^libgamin-1 0" \
+	"^libcups 2" \
+	"^librsvg-2 2" \
+	"^libgtk-x11-2.0 0" \
+	"^libgnomeui-2 0" \
+	; do
+	grep --no-filename "$SONAME" /var/lib/dpkg/info/*.shlibs >> debian/shlibs.local.new || echo "ERROR: could not resolve $SONAME"
+done
 
 cp debian/shlibs.local.new debian/shlibs.local
 
