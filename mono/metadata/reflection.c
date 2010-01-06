74f74432b01dfb0d28d19302461a573b59e57c20
Jo Shields <directhex@apebox.org>
2010-01-06 16:58:17 +0000

    Backport fix from upstream which causes IKVM to fail to build (Closes: #562393)
    Backport of upstream SVN revision r146984. Stops IKVM from crashing
    during compilation. This was a regression between 2.4 and 2.4.2

--- mono/metadata/reflection.c
+++ mono/metadata/reflection.c
@@ -2586,16 +2586,29 @@ is_field_on_inst (MonoClassField *field)
 static MonoType*
 get_field_on_inst_generic_type (MonoClassField *field)
 {
+	MonoClass *class, *gtd;
 	MonoDynamicGenericClass *dgclass;
 	int field_index;
 
 	g_assert (is_field_on_inst (field));
 
 	dgclass = (MonoDynamicGenericClass*)field->parent->generic_class;
-	field_index = field - dgclass->fields;
 
-	g_assert (field_index >= 0 && field_index < dgclass->count_fields);
-	return dgclass->field_generic_types [field_index];
+	if (field >= dgclass->fields && field - dgclass->fields < dgclass->count_fields) {
+		field_index = field - dgclass->fields;
+		return dgclass->field_generic_types [field_index];		
+	}
+
+	class = field->parent;
+	gtd = class->generic_class->container_class;
+
+	if (field >= class->fields && field - class->fields < class->field.count) {
+		field_index = field - class->fields;
+		return gtd->fields [field_index].type;
+	}
+
+	g_assert_not_reached ();
+	return 0;
 }
 
 static guint32
