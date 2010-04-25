5f82dd23a5eaaaf4d70bad36cafb59cb7310a880
Mirco Bauer <meebey@meebey.net>
2010-04-25 14:06:11 +0200

    Fixed NRE when using the attach parameter of csharp.

--- mcs/tools/csharp/repl.cs
+++ mcs/tools/csharp/repl.cs
@@ -199,7 +199,7 @@ namespace Mono {
 
 		public int ReadEvalPrintLoop ()
 		{
-			if (startup_files.Length == 0)
+			if (startup_files != null && startup_files.Length == 0)
 				InitTerminal ();
 
 			InitializeUsing ();
